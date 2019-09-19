19b84d42e5ffcccb0fa5971196a909a2353dff18
James Beattie <jamesbeattie@farmdrop.co.uk>
2019-09-19 11:27:38 +0100

    Tidy up the Package.swift file

--- Package.swift
+++ Package.swift
@@ -5,24 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "Damocles",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Damocles",
-            targets: ["Damocles"]),
+        .library(name: "Damocles", targets: ["Damocles"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Damocles",
-            dependencies: []),
-        .testTarget(
-            name: "DamoclesTests",
-            dependencies: ["Damocles"]),
+        .target(name: "Damocles", dependencies: []),
+        .testTarget(name: "DamoclesTests", dependencies: ["Damocles"]),
     ]
 )
