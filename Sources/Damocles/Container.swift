ee216dc7b48a186c26c3c6bd32239df6811fe057
James Beattie <jamesbeattie@farmdrop.co.uk>
2019-09-19 12:20:31 +0100

    Allow ability to supply specific type for Registration

--- Sources/Damocles/Container.swift
+++ Sources/Damocles/Container.swift
@@ -26,11 +26,27 @@ public struct Container: Resolver {
     public func register<ServiceType>(instance: ServiceType) -> Container {
         return register { _ in instance }
     }
+    
+    /// Register any global instances of objects that you need in your application
+    public func register<ServiceType>(_ type: ServiceType.Type, instance: ServiceType) -> Container {
+        return register(type) { _ in instance }
+    }
 
     /// Register a factory to generate a new instance of the provided class on each invocation
     /// - Parameter factory: A closure that has access to all previously registered types that returns the generic ServiceType
     public func register<ServiceType>(_ factory: @escaping (Resolver) -> ServiceType) -> Container {
         assert(!factories.contains(where: { $0.supports(ServiceType.self) }))
+
+        let newFactory = BasicServiceFactory<ServiceType>(factory: { resolver in
+            factory(resolver)
+        })
+        return .init(factories: factories + [AnyServiceFactory(newFactory)])
+    }
+
+    /// Register a factory to generate a new instance of the provided class on each invocation
+    /// - Parameter factory: A closure that has access to all previously registered types that returns the generic ServiceType
+    public func register<ServiceType>(_ type: ServiceType.Type, _ factory: @escaping (Resolver) -> ServiceType) -> Container {
+        assert(!factories.contains(where: { $0.supports(type) }))
 
         let newFactory = BasicServiceFactory<ServiceType>(factory: { resolver in
             factory(resolver)
