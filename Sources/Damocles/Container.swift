4d087ef61c319e625364a238ab39e81df4d24303
James Beattie <jamesbeattie@farmdrop.co.uk>
2019-09-19 13:08:38 +0100

    Revert previous change

--- Sources/Damocles/Container.swift
+++ Sources/Damocles/Container.swift
@@ -35,7 +35,7 @@ public struct Container: Resolver {
     /// Register a factory to generate a new instance of the provided class on each invocation
     /// - Parameter factory: A closure that has access to all previously registered types that returns the generic ServiceType
     public func register<ServiceType>(_ factory: @escaping (Resolver) -> ServiceType) -> Container {
-        guard !factories.contains(where: { $0.supports(ServiceType.self) }) else { return self }
+        assert(!factories.contains(where: { $0.supports(ServiceType.self) }))
 
         let newFactory = BasicServiceFactory<ServiceType>(factory: { resolver in
             factory(resolver)
@@ -46,7 +46,7 @@ public struct Container: Resolver {
     /// Register a factory to generate a new instance of the provided class on each invocation
     /// - Parameter factory: A closure that has access to all previously registered types that returns the generic ServiceType
     public func register<ServiceType>(_ type: ServiceType.Type, _ factory: @escaping (Resolver) -> ServiceType) -> Container {
-        guard !factories.contains(where: { $0.supports(type) }) else { return self }
+        assert(!factories.contains(where: { $0.supports(type) }))
 
         let newFactory = BasicServiceFactory<ServiceType>(factory: { resolver in
             factory(resolver)
